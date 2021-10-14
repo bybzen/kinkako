3bae72222c603a86ee2b009599ad4628f908dc1a
Thankorn Rattrakulthip <thanakorn.ratt@ku.th>
2021-10-15 00:26:46 +0700

    kinkako

---
+++ src/main/java/th/ac/ku/kinkako/KinkakoApplication.java
@@ -0,0 +1,13 @@
+package th.ac.ku.kinkako;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinkakoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinkakoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/th/ac/ku/kinkako/KinkakoApplicationTests.java
@@ -0,0 +1,13 @@
+package th.ac.ku.kinkako;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinkakoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
